3840437c8c319d9c97b1b4c85f791b2c0cb781ff
devXcant <devxcant@gmail.com>
2025-01-28 08:29:29 +0100

    prisma and mongoose setup

--- src/app/(routes)/settings/page.tsx
+++ src/app/(routes)/settings/page.tsx
@@ -1,11 +1,33 @@
+import { auth } from "@/auth";
+import { prisma } from "@/db";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import { CloudUploadIcon } from "lucide-react";
+import { redirect } from "next/navigation";
 
-export default function SettingsPage() {
+export default async function SettingsPage() {
+  const session = await auth();
   return (
     <div className="max-w-md mx-auto">
       <h1 className="text-2xl font-bold mb-4 text-center">Profile Settings</h1>
-      <form action="">
+      <form
+        action={async (data: FormData) => {
+          "use server";
+          await prisma.profile.upsert({
+            where: {
+              email: session?.user?.email || "",
+            },
+            update: {
+              username: data.get("username") as string,
+              },
+              create: {
+                  email: session?.user?.email || "",
+              username: data.get("username") as string,
+
+              }
+          });
+                  redirect('/profile')
+        }}
+      >
         <div className="flex gap-4 items-center">
           <div>
             <div className="bg-gray-400 size-24 rounded-full"></div>
@@ -18,7 +40,7 @@ export default function SettingsPage() {
           </div>
         </div>
         <p className="mt-2 font-bold">Username:</p>
-        <TextField.Root placeholder="username" />
+        <TextField.Root name="username" placeholder="username" />
         <p className="mt-2 font-bold">Name:</p>
         <TextField.Root placeholder="name" />
         <p className="mt-2 font-bold">Subtitle:</p>
